2422d88cd6710ca8a4a33d5c663a27ec33658ccc
Levi-Huynh <Levi_huynh@yahoo.com>
2019-06-19 19:41:35 -0700

    added db variable to env file

--- data/db.js
+++ data/db.js
@@ -1,4 +1,5 @@
 const knex = require('knex');
+require('dotenv').config();
 
 const dbEngine = process.env.DB || 'development';
 const config = require('../knexfile.js')[dbEngine];
@@ -6,4 +7,4 @@ const config = require('../knexfile.js')[dbEngine];
 // const config = require('../knexfile.js')['development'];
 // const config = require('../knexfile.js')['production']; runs different things from knex file
 
-module.exports = knex(config);
+module.exports = require('knex')(config);
