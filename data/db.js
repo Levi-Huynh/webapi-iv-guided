eb62d89e3764126b76d323b1c57cdbb911a67ef2
Levi-Huynh <Levi_huynh@yahoo.com>
2019-06-19 20:23:41 -0700

    troubleshoot

--- data/db.js
+++ data/db.js
@@ -2,9 +2,9 @@ const knex = require('knex');
 
 
 const dbEngine = process.env.DB || 'development';
-const config = require('../knexfile.js')[dbEngine];
+const config = require('../knexfile.js');
 
 // const config = require('../knexfile.js')['development'];
 // const config = require('../knexfile.js')['production']; runs different things from knex file
 
-module.exports= knex(config);
+module.exports= knex(config[dbEngine]);
